712fee1d22a5bd8f3803343fc05f82a29d71e4ce
Lolito Lindsey <87788112+Lolito-Lindsey-Official@users.noreply.github.com>
2023-10-13 17:50:38 +0000

    made form submission edits

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const submitHandler = (event) => {
+    // prevent the form from reloading automatically
+    event.preventDefault();
+    console.log("The form was submitted!");
+}
 
+const main = () => {
+    // Get the form element
+    const form = document.querySelector("#park-form");
+
+    //Attach the submit handler
+    form.addEventListener("submit", submitHandler);
+};
+
+//Add an event listener
+window.addEventListener("DOMContentLoaded", main);
